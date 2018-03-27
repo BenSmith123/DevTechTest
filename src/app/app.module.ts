02b80a8085ce3a4a29b8f95ff443d42108617180
benjiiman12@gmail.com <benjiiman12@gmail.com>
2018-03-27 22:11:32 +1300

    added get-appointment-by-ID function

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,15 +1,17 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { HttpClientModule} from '@angular/common/http';
+import { FormsModule} from '@angular/forms';
 
 
 import { AppComponent } from './app.component';
 import { GetRecordsComponent } from './get-records/get-records.component';
+import { CreateAppointmentComponent } from './create-appointment/create-appointment.component';
 
 
 @NgModule({
-  declarations: [AppComponent, GetRecordsComponent],
-  imports: [BrowserModule, HttpClientModule],
+  declarations: [AppComponent, GetRecordsComponent, CreateAppointmentComponent],
+  imports: [BrowserModule, HttpClientModule, FormsModule],
   providers: [],
   bootstrap: [AppComponent]
 })
--- src/app/get-records/get-records.component.ts
+++ src/app/get-records/get-records.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit } from '@angular/core';
-import { HttpClient } from '@angular/common/http';
+import { HttpClient, HttpParams } from '@angular/common/http';
 import { Post } from '../post-interface';
 import { Observable } from 'rxjs/Observable';
 
@@ -13,10 +13,14 @@ export class GetRecordsComponent implements OnInit {
   //readonly ROOT_URL = 'https://jsonplaceholder.typicode.com';
   readonly ROOT_URL = 'http://devtechtest.previewourapp.com/api/Appointment?providerEmail=benjiiman12@gmail.com';
 
+//http://devtechtest.previewourapp.com/api/Appointment?providerEmail=benjiiman12@gmail.com&id=4
+
+  myText= "hey";
   posts: Observable<any>; // type = Observable array of Post objects that follow the post-interface
   //posts: any;
 
   constructor(private http: HttpClient) { }
+
 
   ngOnInit(){
   }
@@ -24,12 +28,17 @@ export class GetRecordsComponent implements OnInit {
   getPosts(){
     console.log(this.ROOT_URL);
     this.posts = this.http.get(this.ROOT_URL); // working with posts as any TYPE
+
     //this.posts = this.http.get<Post[]>(this.ROOT_URL);
   }
 
   getPostByID(){
+
+    let params = new HttpParams().set('id','705');
     console.log("getPostByID called");
-    //this.posts = this.http.post(this.ROOT_URL+)
+    this.posts = this.http.get("http://devtechtest.previewourapp.com/api/Appointment?providerEmail=benjiiman12@gmail.com&id=705");
+    //this.posts = this.http.get(this.ROOT_URL+"&id=705");
+
   }
 
 }
